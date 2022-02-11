8355d95733c9ecf2c5f2c5ebce68fb906c955be5
Soheab Khan <skhangns@gmail.com>
2022-02-11 18:15:04 -0500

    updated functionality

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-$("#search-button").on("click",function(){
+$("#search-button").on("click",function(e){
+  e.preventDefault()
     var searchValue = $("#search-value").val();
     console.log(searchValue);
-    $("#search-value").empty()
+    
      searchWeather(searchValue)
     addhistory(searchValue)
-   
+    console.log(searchValue)
+    $("#search-value").empty()
 }); 
+
+
+
+
 $(".searchhistory").on("click", "li", function(){
   searchWeather($(this).text())
 })
@@ -14,41 +20,20 @@ function addhistory(text){
   $(".searchhistory").append(historyItem)
   $("#today").empty();
 }
-function searchWeather(searchValue){
-  
-$.ajax({
-    type: "GET",
-    url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=33c16f35db7793ff40e8bbc19978696f&units=imperial",
-    }).then(data=>{
-        console.log(data)
-        $("#search-value").empty()
-      // clear any old content
-      
-      // create html content for current weather
-      var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
-      var card = $("<div>").addClass("card");
-      var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
-      var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
-      var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
-      var cardBody = $("<div>").addClass("card-body");
-      var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-      // merge and add to page
-      title.append(img);
-      cardBody.append(title, temp, humid, wind);
-      card.append(cardBody);
-      $("#today").append(card);
-      // call follow-up api endpoints
-     
-    }
-  );
-}
+
 function searchWeather(searchValue){
     $.ajax({
         type: "GET",
-        url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=33c16f35db7793ff40e8bbc19978696f&units=imperial",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=33c16f35db7793ff40e8bbc19978696f&units=imperial",
         }).then(data=>{
             console.log(data)
-    
+          
+            if(searchStorage.indexOf(searchValue)=== -1){
+              searchStorage.push(searchValue)
+              window.localStorage.setItem('search-history',JSON.stringify(searchStorage))
+              addhistory(searchValue)
+              }
+
           // clear any old content
           $("#today").empty();
           // create html content for current weather
@@ -96,3 +81,8 @@ function searchWeather(searchValue){
             }
           );
         }
+          var searchStorage = JSON.parse(window.localStorage.getItem("search-history")) || []
+          searchWeather(searchStorage[searchStorage.length-1])
+          for(var i = 0; i < searchStorage.length; i++){
+          addhistory(searchStorage[i])
+ }
